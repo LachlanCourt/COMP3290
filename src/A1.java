f25559a039d09eb71785550b98435804301205f7
LachlanCourt <lachlan.m.court@gmail.com>
2022-07-29 14:09:07 +1000

    Add test files

--- src/A1.java
+++ src/A1.java
@@ -1,12 +1,26 @@
+import java.io.File;
+
 public class A1 {
     public static void main (String[] args) {
         A1 a = new A1();
         if (a.validateArgs(args)) {
             a.run(args);
         }
+        else {
+            System.err.println("Invalid Arguments. Please specify a filename to compile");
+            System.exit(1);
+        }
     }
 
     public boolean validateArgs(String[] args) {
+        if (args.length < 1) {
+            return false;
+        }
+        File f = new File(args[0]);
+        if (!f.exists())
+        {
+            return false;
+        }
         return true;
     }
 

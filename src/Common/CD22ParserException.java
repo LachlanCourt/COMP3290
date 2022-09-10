3d725280b1c7d4938b26d4f75c906b137d846a17
LachlanCourt <lachlan.m.court@gmail.com>
2022-09-10 14:33:43 +1000

    Add comments to exceptions

--- src/Common/CD22ParserException.java
+++ src/Common/CD22ParserException.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ ****    COMP3290 Assignment 2
+ ****    c3308061
+ ****    Lachlan Court
+ ****    10/09/2022
+ ****    Exception for various parsing errors throughout the Parser's recursive descent algorithm to allow jumping to
+ ****    specific areas of the parse
+ *******************************************************************************/
 package Common;
-
 public class CD22ParserException extends Exception {}
